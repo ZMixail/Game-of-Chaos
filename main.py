4ccfac6464afce8b7103a64900341d53ed5f11ac
Mykhailo Lupeiko <41321361+ZMixail@users.noreply.github.com>
2019-11-02 02:48:21 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import pygame
+from random import choice
+
+def game():
+    global dot, pos
+    new_dot = ((pos[0][0]+pos[1][0])//2, (pos[0][1]+pos[1][1])//2)
+    rand_dot = choice(dot)
+    pos = (new_dot, rand_dot)
+    pygame.draw.rect(screen, white, (pos[0][0], pos[0][1], 2, 2))
+
+
+white     = (255, 255, 255)
+black     = (0, 0, 0)
+red       = (255, 0, 0)
+green     = (0, 255, 0)
+blue      = (0, 0, 255)
+
+width = 820
+height = 600
+fps = 60
+dot = []
+
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+screen.fill(black)
+pygame.display.set_caption('Game of Chaos')
+clock = pygame.time.Clock()
+# font = pygame.font.SysFont('Comic Sans MS', 30)
+# textsurface = font.render('Some Text', False, white)
+
+play = False
+run = True
+while run:
+    clock.tick(fps)
+    mouse_pos = pygame.mouse.get_pos()
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            run = False
+        elif e.type == pygame.MOUSEBUTTONDOWN:
+            if len(dot) < 3:
+                pygame.draw.rect(screen, white, (mouse_pos[0], mouse_pos[1], 2, 2))
+                dot.append(mouse_pos)
+            else:
+                if play:
+                    play = False
+                else:
+                    play = True
+
+    if len(dot) == 2:
+        start = dot[0]
+        rand_dot = dot[1]
+        pos = (start, rand_dot)
+    if len(dot) == 3:
+        play = True
+    if play:
+        game()
+    pygame.display.update()
+
+pygame.quit()
